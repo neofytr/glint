0834c236aee0d3fcbb92c64012e43037a571a05d
neofytr <imagination12344321.com@gmail.com>
2025-07-21 16:00:32 +0530

    vector utility functions

--- vector/vector.h
+++ vector/vector.h
@@ -2,6 +2,8 @@
 #define VECTOR_H
 
 #include <cmath>
+#include <iostream>
+
 class Vector {
     public:
         double coords[3]; // (x, y, z)
@@ -12,9 +14,9 @@ class Vector {
         Vector(double x, double y) : coords{x, y, 0.0} {}
         
         // getter functions
-        double x() const {return coords[0];}
-        double y() const {return coords[1];}
-        double z() const {return coords[2];}
+        inline double x() const {return coords[0];}
+        inline double y() const {return coords[1];}
+        inline double z() const {return coords[2];}
 
         // overloading operators for Vector class
         Vector operator-() const {return Vector{-coords[0], -coords[1], -coords[2]};}
@@ -46,5 +48,53 @@ class Vector {
             return std::sqrt(lengthSquared());
         }
 };
+
+using Point = Vector; // point is just an alias for vector internally
+
+// vector utility functions
+inline std::ostream &operator<<(std::ostream &out, const Vector &vec) {
+    return out << vec.x() << " " << vec.y() << " " << vec.z() << "\n";
+}
+
+inline Vector operator+(const Vector &v, const Vector &u) {
+    return Vector(v.x() + u.x(), v.y() + u.y(), v.z() + u.z());
+}
+
+inline Vector operator-(const Vector &v, const Vector &u) {
+    return Vector(v.x() - u.x(), v.y() - u.y(), v.z() - u.z());
+}
+
+inline Vector operator*(const Vector &v, const Vector &u) {
+    return Vector(v.x() * u.x(), v.y() * u.y(), v.z() * u.z());
+}
+
+// two separate so that * works both ways (t * v and v * t)
+inline Vector operator*(const double scaleFactor, const Vector &vec) {
+    return Vector(vec.x() * scaleFactor, vec.y() * scaleFactor, vec.z() * scaleFactor);
+}
+
+inline Vector operator*(const Vector &vec, const double scaleFactor) {
+    return Vector(vec.x() * scaleFactor, vec.y() * scaleFactor, vec.z() * scaleFactor);
+}
+
+inline Vector operator/(const Vector &vec, const double scaleFactor) {
+    return vec * (1 / scaleFactor);
+}
+
+inline double dot(const Vector &v, const Vector &u) {
+    return v.x() * u.x() + v.y() * u.y() + v.z() * u.z();
+}
+
+inline Vector cross(const Vector &v, const Vector &u) {
+    return Vector(
+        v.y() * u.z() - v.z() * u.y(),
+        v.z() * u.x() - v.x() * u.z(),
+        v.x() * u.y() - v.y() * u.x()
+    );
+}
+
+inline Vector unitVector(const Vector &vec) {
+    return vec / vec.length();
+}
 
 #endif // VECTOR_H
