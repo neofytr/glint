85b46fdb21ef735ca12cc5cf6f06e8f3de1d85ef
neofytr <imagination12344321.com@gmail.com>
2025-07-21 20:23:30 +0530

    updated neo.c; added clang format

--- neo.c
+++ neo.c
@@ -1,23 +1,77 @@
 #include "buildsysdep/neobuild.h"
+#include <stdio.h>
+#include <string.h>
 
 #define BIN "bin/"
-#define VECTOR "vector/"
-#define RAY "ray/"
-#define COLOR "color/"
+#define SRC "source/"
+#define INC "include/"
+#define VECTOR SRC "vector/"
+#define RAY SRC "ray/"
+#define COLOR SRC "color/"
 
-#define COMPILER_FLAGS NULL
-#define LINKER_FLAGS NULL
+#define MAX_BUFLEN (2048 + 1)
 
-int main(int argc, char **argv)
-{
+#define CXXFLAGS "-O2 -Wall -Wextra -std=c++17 -I" INC
+#define LFLAGS NULL
+
+int main(int argc, char **argv) {
+    neocmd_t *cmd, *tmp;
+    char      buf[MAX_BUFLEN];
     neorebuild("neo.c", argv, &argc);
 
-    neo_compile_to_object_file(GPP, VECTOR "vector.cpp", BIN "vector.o", COMPILER_FLAGS, false); 
-    neo_compile_to_object_file(GPP, RAY "ray.cpp", BIN "ray.o", COMPILER_FLAGS, false);
-    neo_compile_to_object_file(GPP, COLOR "color.cpp", BIN "color.o", COMPILER_FLAGS, false);
-    neo_compile_to_object_file(GPP, "main.cpp", BIN "main.o", COMPILER_FLAGS, false);
+    if (argc > 1 && !strcmp(argv[1], "clean")) {
+        cmd = neocmd_create(BASH);
+        neocmd_append(cmd, "rm -f " BIN "*.o " BIN "main");
+        neocmd_run_sync(cmd, NULL, NULL, false);
+        neocmd_delete(cmd);
+        return EXIT_SUCCESS;
+    }
 
-    neo_link(GPP, "main", LINKER_FLAGS, false, BIN "main.o", BIN "vector.o", BIN "ray.o", BIN "color.o");
+    if (argc > 1 && !strcmp(argv[1], "push")) {
+        if (argc < 3) {
+            printf("Usage: %s push \"commit message\"\n", argv[0]);
+            return EXIT_FAILURE;
+        }
+
+        snprintf(buf, MAX_BUFLEN - 1, "git commit -m \"%s\" &&", argv[2]);
+        buf[MAX_BUFLEN - 1] = 0;
+
+        cmd = neocmd_create(BASH);
+        neocmd_append(cmd, "./neo clean");
+        neocmd_run_sync(cmd, NULL, NULL, false);
+
+        tmp = neocmd_create(BASH);
+        neocmd_append(tmp, "git add . &&");
+        neocmd_append(tmp, buf);
+        neocmd_append(tmp, "git push -u origin main");
+
+        neocmd_run_sync(tmp, NULL, NULL, false);
+        neocmd_delete(cmd);
+        neocmd_delete(tmp);
+        return EXIT_SUCCESS;
+    }
+
+    neo_compile_to_object_file(GPP, VECTOR "vector.cpp", BIN "vector.o", CXXFLAGS, false);
+    neo_compile_to_object_file(GPP, RAY "ray.cpp", BIN "ray.o", CXXFLAGS, false);
+    neo_compile_to_object_file(GPP, COLOR "color.cpp", BIN "color.o", CXXFLAGS, false);
+    neo_compile_to_object_file(GPP, SRC "main.cpp", BIN "main.o", CXXFLAGS, false);
+
+    neo_link(GPP, BIN "main", LFLAGS, false, BIN "main.o", BIN "vector.o", BIN "ray.o",
+             BIN "color.o");
+
+    if (argc > 1 && !strcmp(argv[1], "dis")) {
+        tmp = neocmd_create(BASH);
+        neocmd_append(tmp, "objdump -d -M intel", BIN "main");
+        neocmd_run_sync(tmp, NULL, NULL, false);
+        neocmd_delete(tmp);
+    }
+
+    if (argc > 1 && !strcmp(argv[1], "run")) {
+        tmp = neocmd_create(BASH);
+        neocmd_append(tmp, "./", BIN "main");
+        neocmd_run_sync(tmp, NULL, NULL, false);
+        neocmd_delete(tmp);
+    }
 
     return EXIT_SUCCESS;
 }
